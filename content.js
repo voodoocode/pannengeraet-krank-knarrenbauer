b63090ec593ab47f0ffe927e2bb8f26b7a3ed50e
Viktor Seč <hey@viktorsec.com>
2016-11-11 19:53:02 +0100

    Add substitutions

--- content.js
+++ content.js
@@ -1,7 +1,31 @@
 var substitutions = [
 	'Bumpercar Candysnatch',
 	'Bendypole Cabbagepatch',
-	'Beautiful PlumbersCrack'
+	'Beautiful Plumberscrack',
+	'Bumblebee Baggage',
+	'Bendover Cumonback',
+	'Benadryl Cucumberpot',
+	'Wimbledon Tennismatch',
+	'Clumpity Bandersnatch',
+	'Benefish Cumberface',
+	'Bembledump Melonpatch',
+	'Buildabear Bumbleswatch',
+	'Benny Cummerbund',
+	'Bandicoot Cumbercrash',
+	'Fiddledick Bandersnatch',
+	'Beneful Thundercrunch',
+	'Bundersnatch Cabbagewank',
+	'Bendydick Cuminsnatch',
+	'Bumperbadge Cabbagepatch',
+	'Beedlebop Cootersnooch',
+	'Bandersnatch Crumblebum',
+	'Benderling Crumblewrench',
+	'Viaduct Plumberthatch',
+	'Camperdown Bramblepatch',
+	'Benelux Bandersnatch',
+	'Benedict Bumberwatch',
+	'Humperdinck Cummerbund',
+	'Derelict Slumbersnatch',
 ];
 
 var elements = document.getElementsByTagName('*');
@@ -14,7 +38,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Benedict Cumberbatch/gi, substitutions[Math.floor(Math.random() * substitutions.length)]);
+            var replacedText = text.replace(/Benedict Cumberbatch|Cumberbatch/gi, substitutions[Math.floor(Math.random() * substitutions.length)]);
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
